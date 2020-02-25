912a916531ea25cf6a57d84a16430026590a5a62
duydodevgithub <duydo.dev@gmail.com>
2020-02-25 02:58:43 -0600

    remove add,remove question action

--- src/actions/Question.js
+++ src/actions/Question.js
@@ -5,36 +5,36 @@ import { _saveQuestionAnswer } from "../utils/_DATA";
 import {showLoading, hideLoading} from "react-redux-loading";
 
 //add new question
-const addQuestionAction = (question) => {
-    // console.log("hereeee");
-    return {
-        type: 'ADD_QUESTION',
-        question: {
-            id: uuid(),
-            author: question.author,
-            optionOneText: question.optionOneText,
-            optionTwoText: question.optionTwoText
-        }
-    }
-}
+// const addQuestionAction = (question) => {
+//     // console.log("hereeee");
+//     return {
+//         type: 'ADD_QUESTION',
+//         question: {
+//             id: uuid(),
+//             author: question.author,
+//             optionOneText: question.optionOneText,
+//             optionTwoText: question.optionTwoText
+//         }
+//     }
+// }
 
-const removeQuestionAction = (question) => {
-    return {
-        type: "REMOVE_QUESTION",
-        question: {
-            id: question.id
-        }
-    }
-}
+// const removeQuestionAction = (question) => {
+//     return {
+//         type: "REMOVE_QUESTION",
+//         question: {
+//             id: question.id
+//         }
+//     }
+// }
 
 export function handleAddQuestion(question) {
     console.log("From action handleAddQuestion: ", question);
     return (dispatch) => {
         dispatch(showLoading());
-        dispatch(addQuestionAction(question));
+        // dispatch(addQuestionAction(question));
         saveQuestion(question)
         .catch(() => {
-            dispatch(removeQuestionAction(question));
+            // dispatch(removeQuestionAction(question));
             alert("Error saving question. Please try again");
         }).then(() =>{
             dispatch(handleLoadInitialData());
