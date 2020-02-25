a1a387628e6aefd34187600f7ea4345191867512
DUY DO <danny@imprint.com>
2020-02-24 19:39:42 -0600

    finish sort leader board

--- src/components/QuestionDetails/QuestionDetails.js
+++ src/components/QuestionDetails/QuestionDetails.js
@@ -2,7 +2,13 @@ import React from "react";
 import {connect} from "react-redux";
 
 class QuestionDetails extends React.Component {
-
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
 }
 
 const mapStateToProps();
